145ff19756c36d43d3d2c99dec6f681e8c6dac70
yt-free-proxy <faridmay09@gmail.com>
2025-06-11 04:20:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
-app.use('/', createProxyMiddleware({
-  target: 'https://redirector.googlevideo.com',
-  changeOrigin: true
+app.use("/", createProxyMiddleware({
+  target: "https://redirector.googlevideo.com",
+  changeOrigin: true,
+  secure: false,
+  ws: true,
+  pathRewrite: {
+    "^/": "/",
+  },
+  headers: {
+    Host: "redirector.googlevideo.com",
+    "User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"
+  }
 }));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(8080, () => {
+  console.log("Proxy running on port 8080");
 });
